2b989526d79af9050514a78e21e74bb560393fa5
Bruno  Ferreira <bruno@contabilidadecastroalves.com.br>
2025-07-16 19:01:35 -0300

    V: Esqueci Senha 6.0

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -43,7 +43,7 @@ const Login = () => {
   }
 
   try {
-    const response = await fetch(`${apiUrl}/esqueci-senha`, {
+    const response = await fetch(`${apiUrl}/redefinir-senha`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
